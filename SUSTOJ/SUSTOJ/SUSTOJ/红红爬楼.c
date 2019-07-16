49c3e2b2ead24146dc68701e95b66873fe1f4598
Mbabysbreath <1830817543@qq.com>
2019-07-16 23:48:47 +0800

    递归2

---
+++ SUSTOJ/SUSTOJ/SUSTOJ/红红爬楼.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   1
+#include<stdio.h>
+#include<stdlib.h>
+int Floor(int n){
+	if (n == 1){
+		return 1;
+	}
+	else if (n == 2){
+		return 2;
+	}
+	if (n == 0){
+		return 0;
+	}
+	else{
+		return Floor(n - 1) + Floor(n - 2);
+	}
+}
+int main(){
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d", Floor(n));
+	system("pause");
+	return 0;
+}
